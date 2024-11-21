0f66257212e1142433a0ca5ddc2e1eba3be26aab
BIG <bigguitarsolo@gmail.com>
2024-11-22 03:26:57 +0700

    Add Jest plugin and update ESLint configuration
    Added `eslint-plugin-jest` to manage Jest-specific linting rules.
    Revised ESLint configuration to improve style enforcement and manage
    unused imports. Updated ignore patterns to exclude config files and
    build artifacts.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,15 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
+import jestPlugin from 'eslint-plugin-jest';
 
 export default tseslint.config({
   files: ['{src,apps,libs,test}/**/*.ts'],
-  extends: [eslint.configs.recommended, tseslint.configs.recommended],
+  extends: [
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    tseslint.configs.stylistic,
+  ],
   languageOptions: {
     parser: tseslint.parser,
     parserOptions: {
@@ -12,7 +18,41 @@ export default tseslint.config({
       tsconfigRootDir: import.meta.dirname,
     },
   },
+  plugins: {
+    '@typescript-eslint': tseslint.plugin,
+    'unused-imports': unusedImports,
+    jest: jestPlugin,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
   rules: {
     '@typescript-eslint/array-type': 'error',
+    'no-unused-vars': 'off',
+    'no-return-await': 'error',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
   },
+  ignores: [
+    '**/node_modules/**',
+    '**/dist/**',
+    '**/coverage/**',
+    '**/*.config.mjs',
+    '**/*.config.js',
+    'package.json',
+  ],
 });
