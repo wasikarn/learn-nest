5b4e6c6895c229fe9dc2175a2474d2545335c115
BIG <bigguitarsolo@gmail.com>
2024-11-25 12:59:30 +0700

    Remove ExceptionController from the project
    Deleted ExceptionController and its route handling logic from the
    project. Also updated AppModule to reflect the removal by excluding
    ExceptionController from the controller list.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { ParametersController } from '@/parameters.controller';
-import { ExceptionController } from '@/exception.controller';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [PostsModule],
-  controllers: [AppController, ParametersController, ExceptionController],
+  controllers: [AppController, ParametersController],
   providers: [AppService],
 })
 export class AppModule {}
