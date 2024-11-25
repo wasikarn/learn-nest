b6a1d4b42e98f2d1dae43a9e7f6cb31b2f00d8a9
BIG <bigguitarsolo@gmail.com>
2024-11-25 16:11:34 +0700

    Refactor fetchPostsEffect to use pipe with Effect
    This change refactors the `fetchPostsEffect` method to use the
    `pipe` function with `Effect` for better readability and maintainability.
    The modification splits the promise handling and data extraction into
    separate, more manageable steps.

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
 import { Post } from '@/posts/posts.interface';
-import { Effect } from 'effect';
+import { Effect, pipe } from 'effect';
 import { AxiosResponse } from 'axios';
 
 @Injectable()
@@ -20,15 +20,19 @@ export class PostsService {
   }
 
   async fetchPostsEffect(): Promise<Post[]> {
-    const postsEffect = Effect.tryPromise({
-      try: (): PromiseLike<Post[]> =>
-        this.httpService.axiosRef
-          .get<Post[]>('https://jsonplaceholder.typicode.com/posts')
-          .then((res: AxiosResponse<Post[]>): Post[] => res.data),
-      catch: (err) => new Error(`fetching posts failed: ${err}`),
-    });
+    const pipeEffect = pipe(
+      Effect.tryPromise(
+        (): Promise<AxiosResponse<Post[]>> =>
+          this.httpService.axiosRef.get<Post[]>(
+            'https://jsonplaceholder.typicode.com/posts',
+          ),
+      ),
+      Effect.flatMap((response: AxiosResponse<Post[]>) =>
+        Effect.try((): Post[] => response.data),
+      ),
+    );
 
-    return Effect.runPromise(postsEffect);
+    return Effect.runPromise(pipeEffect);
   }
 
   *postTitles(posts: Post[]): Generator<string, void, unknown> {
