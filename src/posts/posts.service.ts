4762d6f99fa10d4cb2d15b56d7d092c61367c757
BIG <bigguitarsolo@gmail.com>
2024-11-28 20:34:01 +0700

    Simplify post-fetching logic by removing Effect usage.
    Refactor the fetchPosts method to directly use promises instead of
    Effect and pipe, streamlining the code. This change reduces complexity
    and improves the readability of the post-fetching logic.

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { Effect, pipe } from 'effect';
 import { AxiosResponse } from 'axios';
 import { Post } from './posts.response.dto';
 
@@ -9,18 +8,8 @@ export class PostsService {
   constructor(private readonly httpService: HttpService) {}
 
   async fetchPosts(): Promise<Post[]> {
-    const pipeEffect = pipe(
-      Effect.tryPromise(
-        (): Promise<AxiosResponse<Post[]>> =>
-          this.httpService.axiosRef.get<Post[]>(
-            'https://jsonplaceholder.typicode.com/posts',
-          ),
-      ),
-      Effect.flatMap((response: AxiosResponse<Post[]>) =>
-        Effect.try((): Post[] => response.data),
-      ),
-    );
-
-    return Effect.runPromise(pipeEffect);
+    return this.httpService.axiosRef
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then((response: AxiosResponse<Post[]>): Post[] => response.data);
   }
 }
