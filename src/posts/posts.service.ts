6df09c492123d16586123ac3af54f48f2e622e45
BIG <bigguitarsolo@gmail.com>
2024-11-29 01:30:11 +0700

    Add error handling to fetchPosts and implement effectGetPosts
    Enhance the fetchPosts method with proper error handling using RxJS to
    catch Axios errors and log them. Introduced a new method,
    effectGetPosts, leveraging functional programming constructs for async
    operations, providing more robust error handling and type safety.

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,15 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { AxiosResponse } from 'axios';
+import { AxiosError } from 'axios';
 import { Post } from './posts.response.dto';
+import { Data, Effect } from 'effect';
+import { catchError, firstValueFrom } from 'rxjs';
+
+class RequestError extends Data.TaggedError('RequestError') {}
+class NotOkError extends Data.TaggedError('NotOkError') {}
+class NotJsonError extends Data.TaggedError('NotJsonError') {}
 
 @Injectable()
 export class PostsService {
+  private readonly logger: Logger = new Logger(PostsService.name);
   constructor(private readonly httpService: HttpService) {}
 
   async fetchPosts(): Promise<Post[]> {
-    return this.httpService.axiosRef
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then((response: AxiosResponse<Post[]>): Post[] => response.data);
+    const { data } = await firstValueFrom(
+      this.httpService
+        .get<Post[]>('https://jsonplaceholder.typicode.com/posts')
+        .pipe(
+          catchError((error: AxiosError): never => {
+            this.logger.error(error.response?.data);
+            throw 'An error happened!';
+          }),
+        ),
+    );
+
+    return data;
+  }
+
+  effectGetPosts() {
+    const getPosts = Effect.tryPromise({
+      try: () => fetch('https://jsonplaceholder.typicode.com/posts'),
+      catch: () => new RequestError(),
+    });
+    const effectGenerator = Effect.gen(function* () {
+      const response = yield* getPosts;
+
+      if (!response.ok) {
+        return yield* Effect.fail(new NotOkError());
+      }
+
+      return yield* Effect.tryPromise({
+        try: () => response.json(),
+        catch: () => new NotJsonError(),
+      });
+    });
+
+    return Effect.runPromise(effectGenerator);
   }
 }
