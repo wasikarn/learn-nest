a2f920240ed333f703e3f18bbddf1526208ef23c
BIG <bigguitarsolo@gmail.com>
2024-12-13 10:50:09 +0700

    Add mongoose-autopopulate plugin to Mongoose configuration
    Integrate the mongoose-autopopulate plugin into the Mongoose connection
     factory for automatic population of referenced documents. This
     simplifies data fetching and reduces boilerplate code for handling
     relations in MongoDB documents. Updated package.json to include the
     plugin as a new dependency.

--- src/config/mongoose-config.service.ts
+++ src/config/mongoose-config.service.ts
@@ -4,6 +4,8 @@ import {
   MongooseModuleOptions,
   MongooseOptionsFactory,
 } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+import mongooseAutopopulate from 'mongoose-autopopulate';
 
 export const MONGODB_DEFAULT_CONNECTION = 'default';
 
@@ -13,6 +15,9 @@ export class MongooseConfigService implements MongooseOptionsFactory {
 
   createMongooseOptions(): MongooseModuleOptions {
     return {
+      connectionFactory: (connection: Connection): void => {
+        connection.plugin(mongooseAutopopulate);
+      },
       uri: this.configService.get<string>('MONGODB_URI') as string,
     };
   }
