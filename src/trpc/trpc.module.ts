0fab4c1bdd7b67dd87114abe7f136e8ba4b1c811
BIG <bigguitarsolo@gmail.com>
2024-12-19 20:41:34 +0700

    Add tRPC Panel integration for API visualization
    Introduced a new `TrpcPanelController` to render the tRPC Panel for
    easier API inspection and interaction. Updated the `trpc.module.ts` to
    include the controller and added `trpc-panel` as a dependency in
    `package.json`.

--- src/trpc/trpc.module.ts
+++ src/trpc/trpc.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TRPCModule } from 'nestjs-trpc';
 
+import { TrpcPanelController } from './trpc-panel.controller';
+
 @Module({
   imports: [
     TRPCModule.forRoot({
       autoSchemaFile: 'src/trpc/@generated',
     }),
   ],
+  controllers: [TrpcPanelController],
 })
 export class TrpcModule {}
