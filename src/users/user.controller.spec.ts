9c87e6c078a3c659f83a2cbf685195a91a102155
BIG <bigguitarsolo@gmail.com>
2024-12-20 23:21:18 +0700

    Refactor PaginatedDto and enhance UserController tests
    Renamed "results" to "data" in PaginatedDto for consistency. Added unit
    tests for UserController methods, including `getUsers` and `createUser`,
     with mock implementations for the UserService.

--- src/users/user.controller.spec.ts
+++ src/users/user.controller.spec.ts
@@ -1,13 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { PaginatedDto } from '../common/paginated.dto';
+import { CreateUserRequestDto } from './dto/request/create-user.request.dto';
 import { UserController } from './user.controller';
+import { User } from './user.schema';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  let userServiceMock: Partial<UserService>;
 
   beforeEach(async () => {
+    userServiceMock = {
+      findAll: jest
+        .fn()
+        .mockResolvedValue([{ name: { firstName: 'John', lastName: 'Doe' } }]),
+      createUser: jest
+        .fn()
+        .mockResolvedValue({ name: { firstName: 'Jane', lastName: 'Smith' } }),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [{ provide: UserService, useValue: userServiceMock }],
     }).compile();
 
     controller = module.get<UserController>(UserController);
@@ -15,5 +30,33 @@ describe('UserController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('getUsers', () => {
+    it('should return a paginated list of users', async () => {
+      const result: PaginatedDto<User> = await controller.getUsers();
+
+      expect(result).toBeDefined();
+      expect(result.data).toEqual([
+        { name: { firstName: 'John', lastName: 'Doe' } },
+      ]);
+    });
+  });
+
+  describe('createUser', () => {
+    it('should create a user and return it', async () => {
+      const createUserRequestDto: CreateUserRequestDto = {
+        name: { firstName: 'Jane', lastName: 'Smith' },
+      };
+
+      const result = await controller.createUser(createUserRequestDto);
+
+      expect(userServiceMock.createUser).toHaveBeenCalledWith(
+        createUserRequestDto,
+      );
+      expect(result).toEqual({
+        name: { firstName: 'Jane', lastName: 'Smith' },
+      });
+    });
   });
 });
