9fcc68e6694664607a5e50348f13fbece31c975c
BIG <bigguitarsolo@gmail.com>
2025-01-29 15:16:30 +0700

    Refactor to use NestExpressApplication and enable extended query parser
    Switched from INestApplication to NestExpressApplication to
    leverage-platform-specific features. Enable the extended query parser to
     support more complex query string structures, improving compatibility
     with certain APIs.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
@@ -11,13 +12,22 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { setupSwaggerDocumentation } from './common/swagger';
 
 async function bootstrap(): Promise<void> {
-  const app: INestApplication = await NestFactory.create(AppModule, {
-    bufferLogs: true,
-  });
+  const app: NestExpressApplication =
+    await NestFactory.create<NestExpressApplication>(AppModule, {
+      bufferLogs: true,
+    });
 
   const configService: ConfigService = app.get(ConfigService);
 
   const httpAdapterHost: HttpAdapterHost = app.get(HttpAdapterHost);
+
+  /**
+   * As a result, query strings like these:
+   *
+   * ?filter[where][name]=John&filter[where][age]=30
+   * ?item[]=1&item[]=2
+   */
+  app.set('query parser', 'extended');
 
   app.use(compression());
   app.use(cookieParser());
